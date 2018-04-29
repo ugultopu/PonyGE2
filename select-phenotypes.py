395ee3aba7d3673b3725c965f97a5a2956390329
Utku Gultopu <ugultopu@gmail.com>
2018-04-29 16:18:14 -0300

    Add drawing function

--- select-phenotypes.py
+++ select-phenotypes.py
@@ -1,3 +1,16 @@
+import turtle
+
+def draw_phenotype(phenotype):
+    for move in phenotype:
+        if move == 'F':
+            turtle.forward(100)
+        elif move == 'B':
+            turtle.backward(100)
+        elif move == '+':
+            turtle.left(90)
+        elif move == '-':
+            turtle.right(90)
+
 MAX_PHENOTYPE_LENGTH = 20
 
 phenotypes = []
@@ -17,3 +30,8 @@ with open('/Users/utku/src/python/PonyGE2/results/TA3E3.WPA.Dal.Ca_18_4_27_17312
         shortest_phenotype = min(phenotypes, key=len)
 
 print(phenotypes)
+
+draw_phenotype(phenotypes[0])
+
+# for phenotype in phenotypes:
+#     draw_phenotype(phenotype)
