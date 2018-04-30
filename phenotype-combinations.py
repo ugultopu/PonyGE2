80909644c92a834a0c0801d448ad0b305bcc126d
Utku Gultopu <ugultopu@gmail.com>
2018-04-30 16:58:11 -0300

    Get shorter and longer phenotypes

--- phenotype-combinations.py
+++ phenotype-combinations.py
@@ -1,7 +1,5 @@
 import itertools
 
-phenotypes = []
-
 with open('test.txt') as file:
   phenotypes = file.read().splitlines()
 
@@ -12,4 +10,12 @@ combinations = itertools.combinations(phenotypes, 2)
 # print(list(combinations))
 
 for combo in combinations:
-    print(combo)
+    if len(combo[0]) < len(combo[1]):
+        shorter_phenotype = combo[0]
+        longer_phenotype = combo[1]
+    else:
+        shorter_phenotype = combo[1]
+        longer_phenotype = combo[0]
+
+    print(shorter_phenotype)
+    print(longer_phenotype)
