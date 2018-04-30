f66d3a1b99403710c9587cf85fccfd76f605fc0c
Utku Gultopu <ugultopu@gmail.com>
2018-04-30 17:19:27 -0300

    Get index of 'zip' using 'enumerate'
    https://www.saltycrane.com/blog/2008/04/how-to-use-pythons-enumerate-and-zip-to/

--- phenotype-combinations.py
+++ phenotype-combinations.py
@@ -10,6 +10,7 @@ combinations = itertools.combinations(phenotypes, 2)
 # print(list(combinations))
 
 for combo in combinations:
-    for a, b in zip(combo[0], combo[1]):
-        print(a)
-        print(b)
+    for i, (a, b) in enumerate(zip(combo[0], combo[1])):
+        print('index: ' + str(i))
+        print('first element: ' + str(a))
+        print('second element: ' + str(b))
