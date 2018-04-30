984f8a562bcdc1c75a506fe1e9fe00c09737630d
Utku Gultopu <ugultopu@gmail.com>
2018-04-30 19:12:45 -0300

    Calculate the length of matching and non-matching parts of phenotypes and record them to a list

--- phenotype-combinations.py
+++ phenotype-combinations.py
@@ -5,12 +5,27 @@ with open('test.txt') as file:
 
 # print(phenotypes)
 
-combinations = itertools.combinations(phenotypes, 2)
+combinations = []
 
-# print(list(combinations))
+for combo in itertools.combinations(phenotypes, 2):
+    phenotype_0_length = len(combo[0])
+    phenotype_1_length = len(combo[1])
+    if phenotype_0_length < phenotype_1_length:
+        short_phenotype_length = phenotype_0_length
+        long_phenotype_length = phenotype_1_length
+    else:
+        short_phenotype_length = phenotype_1_length
+        long_phenotype_length = phenotype_0_length
 
-for combo in combinations:
-    for i, (a, b) in enumerate(zip(combo[0], combo[1])):
-        print('index: ' + str(i))
-        print('first element: ' + str(a))
-        print('second element: ' + str(b))
+    for i, (unit_0, unit_1) in enumerate(zip(combo[0], combo[1])):
+        # print('index: ' + str(i))
+        # print('first element: ' + str(unit_0))
+        # print('second element: ' + str(unit_1))
+        if unit_0 != unit_1:
+            combinations.append((*combo, i, short_phenotype_length + long_phenotype_length - 2 * i))
+            break
+        if i == short_phenotype_length - 1:
+            combinations.append((*combo, short_phenotype_length, long_phenotype_length - short_phenotype_length))
+
+for combination in combinations:
+    print(combination)
