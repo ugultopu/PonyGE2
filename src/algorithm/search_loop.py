a465fa72e8aa6070d198b496e6b97f9ecc0c3584
Utku Gultopu <ugultopu@gmail.com>
2018-05-03 19:11:13 -0300

    Save last generation's phenotypes

--- src/algorithm/search_loop.py
+++ src/algorithm/search_loop.py
@@ -1,4 +1,6 @@
+from os import path
 from multiprocessing import Pool
+
 from algorithm.parameters import params
 from fitness.evaluation import evaluate_fitness
 from stats.stats import stats, get_stats
@@ -10,7 +12,7 @@ def search_loop():
     """
     This is a standard search process for an evolutionary algorithm. Loop over
     a given number of generations.
-    
+
     :return: The final population after the evolutionary process has run for
     the specified number of generations.
     """
@@ -36,6 +38,12 @@ def search_loop():
         # New generation
         individuals = params['STEP'](individuals)
 
+        # Save the phenotypes of the valid individuals of the last generation
+        if generation == params['GENERATIONS']:
+            with open(path.join(params['FILE_PATH'], 'last_generation_phenotypes.txt'), 'a') as f:
+                for individual in [i for i in individuals if not i.invalid]:
+                    f.write(individual.phenotype + '\n')
+
     if params['MULTICORE']:
         # Close the workers pool (otherwise they'll live on forever).
         params['POOL'].close()
@@ -51,23 +59,23 @@ def search_loop_from_state():
     :return: The final population after the evolutionary process has run for
     the specified number of generations.
     """
-    
+
     individuals = trackers.state_individuals
-    
+
     if params['MULTICORE']:
         # initialize pool once, if mutlicore is enabled
         params['POOL'] = Pool(processes=params['CORES'], initializer=pool_init,
                               initargs=(params,))  # , maxtasksperchild=1)
-    
+
     # Traditional GE
     for generation in range(stats['gen'] + 1, (params['GENERATIONS'] + 1)):
         stats['gen'] = generation
-        
+
         # New generation
         individuals = params['STEP'](individuals)
-    
+
     if params['MULTICORE']:
         # Close the workers pool (otherwise they'll live on forever).
         params['POOL'].close()
-    
+
     return individuals
