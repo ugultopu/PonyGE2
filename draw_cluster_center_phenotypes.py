58cbee19d2e0830a034e3642cecc03bbf0db2ebd
Utku Gultopu <ugultopu@gmail.com>
2018-05-07 16:50:40 -0300

    Draw all phenotypes of the same cluster at once
    Drew all phenotypes of the same cluster at once without a way of
    indicating that a line has been drawn more than once.

--- draw_cluster_center_phenotypes.py
+++ draw_cluster_center_phenotypes.py
@@ -4,18 +4,22 @@ FILE_PATH = '/Users/utku/src/python/PonyGE2/results/TA3E3.WPA.Dal.Ca_18_5_5_1902
 NUMBER_OF_CLUSTERS = 5
 EVERY_Nth_GENERATION = 10
 
-def draw_phenotype(cluster_idx, phenotype_idx, phenotype):
+def draw_phenotypes(cluster_idx, phenotype_list):
     turtle.reset()
-    for move in phenotype:
-        if move == 'F':
-            turtle.forward(100)
-        elif move == 'B':
-            turtle.backward(100)
-        elif move == '+':
-            turtle.left(90)
-        elif move == '-':
-            turtle.right(90)
-    turtle.getscreen().getcanvas().postscript(file='phenotype_'+str(cluster_idx)+'_'+str(phenotype_idx)+'.eps')
+    for phenotype in phenotype_list:
+        turtle.penup()
+        turtle.home()
+        turtle.pendown()
+        for move in phenotype:
+            if move == 'F':
+                turtle.forward(100)
+            elif move == 'B':
+                turtle.backward(100)
+            elif move == '+':
+                turtle.left(90)
+            elif move == '-':
+                turtle.right(90)
+    turtle.getscreen().getcanvas().postscript(file='phenotype_'+str(cluster_idx)+'.eps')
 
 with open(FILE_PATH) as f:
     phenotypes = f.read().splitlines()
@@ -24,5 +28,4 @@ with open(FILE_PATH) as f:
 phenotypes_for_clusters = [ phenotypes[2*n::2*NUMBER_OF_CLUSTERS][::EVERY_Nth_GENERATION] for n in range(NUMBER_OF_CLUSTERS) ]
 
 for cluster_idx, phenotype_list in enumerate(phenotypes_for_clusters):
-    for phenotype_idx, phenotype in enumerate(phenotype_list):
-        draw_phenotype(cluster_idx, phenotype_idx, phenotype)
+    draw_phenotypes(cluster_idx, phenotype_list)
