21747a716594e4d5444bf1304f2e196bef06f99b
Utku Gultopu <ugultopu@gmail.com>
2018-06-01 14:56:07 -0300

    Remove number of corners from the fitness function

--- src/fitness/l_systems.py
+++ src/fitness/l_systems.py
@@ -33,25 +33,17 @@ class l_systems(base_ff):
         # X and Y position
         position = [0, 0]
         direction = 'EAST'
-        latest_forward_direction = 'EAST'
-        number_of_corners = 0
         max_x = 0
         max_y = 0
         for move in ind.phenotype:
             if move == 'F':
-                # If we start going west, don't evaluate the rest.
-                if direction == 'WEST': break
                 # Get the new position
-                position = [a + b for a, b in zip(position, get_position_difference_for_direction(direction))]
-                max_x = position[0] if position[0] > max_x else max_x
-                max_y = position[1] if position[1] > max_y else max_y
+                position = [cc + nc for cc, nc in zip(position, get_position_difference_for_direction(direction))]
                 # If we end up below the ground or left of the origin, stop.
                 if position[0] < 0 or position[1] < 0: break
-                # Increase the number of corners if we started facing another
-                # direction.
-                if DIRECTIONS.index(direction) % 2 != DIRECTIONS.index(latest_forward_direction) % 2: number_of_corners += 1
-                # Update the latest forward direction.
-                latest_forward_direction = direction
+                # Update the max X and Y values.
+                max_x = position[0] if position[0] > max_x else max_x
+                max_y = position[1] if position[1] > max_y else max_y
             # If we're not moving forward, this means that we're rotating, since
             # the only other options apart from 'F' are '+', '-' or 'T', which
             # correspond to rotating counter-clockwise, rotating clockwise
@@ -64,8 +56,7 @@ class l_systems(base_ff):
                     if e.args[1] == 'T': break
                     else: raise
 
-        current_generation = trackers.current_generation
-        if current_generation < params['GENERATIONS'] * .4: return 1 / ( max_y + number_of_corners )
-        elif current_generation < params['GENERATIONS'] * .6: return 1 / ( max_x + number_of_corners )
-        elif current_generation < params['GENERATIONS'] * .8: return 1 / ( max_y + number_of_corners )
-        else: return 1 / ( max_x + number_of_corners )
+        if trackers.current_generation < params['GENERATIONS'] * .4: return 1 / ( max_y + 1 )
+        elif trackers.current_generation < params['GENERATIONS'] * .6: return 1 / ( max_x + 1 )
+        elif trackers.current_generation < params['GENERATIONS'] * .8: return 1 / ( max_y + 1 )
+        else: return 1 / ( max_x + 1 )
